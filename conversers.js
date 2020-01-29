5ad7861a03c947c16410b4d48af7398ab3ac13a9
Mark Eschbach and Timothy Smith <meschbach@gmail.com>
2020-01-29 13:19:26 -0800

    added conversers for time and year

--- conversers.js
+++ conversers.js
@@ -5,6 +5,22 @@ function addConversers(interpreter){
 	};
 	parrotConverser.examples = ["say hello", "say goodbye", "say no", "say what are you doing", "say moo", "say yes please"];
 	interpreter.registerIntent("say", parrotConverser);
+
+	const timeConverser = (cmd, parsed, talkback) => {
+		const requested = parsed.splice(1);
+		const hour = new Date().getHours();
+		const minutes = new Date().getMinutes();
+		const oh = minutes < 10 ? " O ": "";
+		talkback.say ("the time is " +hour +" " +oh +minutes);
+	};
+	interpreter.registerIntent("time", timeConverser);
+
+	const yearConverser = (cmd, parsed, talkback) => {
+		const requested = parsed.splice(1);
+		const year = new Date().getFullYear();
+		talkback.say ("The year is " +year);
+	};
+	interpreter.registerIntent("year", yearConverser)
 }
 
 module.exports = {
